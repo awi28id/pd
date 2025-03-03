55dc1e5d5b678cc71e304679cfca274adffdad69
awi28id <nurman.wiyarso@gmail.com>
2025-03-04 05:08:53 +0700

    update

--- if_statement.py
+++ if_statement.py
@@ -2,9 +2,11 @@ x = int(input("Masukan nilai x: "))
 y = int(input("Masukan nilai y: "))
 
 if x > y:
-    print("benar! x lebih dari y")
+    print("benar! x lebih besar dari y")
 elif x == y:
     print("x sama dengan y")
+#elif x < y:
+#    print("benar! x lebih kecil dari y")
 else:
     print("anda salah!")
 
